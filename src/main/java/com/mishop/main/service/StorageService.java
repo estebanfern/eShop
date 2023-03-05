7b3044769e28adb9b3c6041f0e93311923aab817
Esteban Fernandez <estebangfernandeza@gmail.com>
2023-03-05 20:02:27 -0300

    application variable path

--- src/main/java/com/mishop/main/service/StorageService.java
+++ src/main/java/com/mishop/main/service/StorageService.java
@@ -10,6 +10,7 @@ import java.sql.Timestamp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,14 +20,13 @@ public class StorageService {
 
     private Logger logger = LoggerFactory.getLogger(StorageService.class);
 
-    private final String FULL_PATH = "C:\\Users\\msi\\Desktop\\eshop\\src\\main\\resources\\static\\img\\data\\";
-
-    private final String SEMI_PATH = "C:\\Users\\msi\\Desktop\\eshop\\src\\main\\resources\\static";
+    @Value("${IMG_DATA_PATH}")
+    private String IMG_DATA_PATH;
 
     public String saveFile(MultipartFile file) throws IOException {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         String fileName = timestamp.toInstant().toEpochMilli() + "." + StringUtils.getFilenameExtension(file.getOriginalFilename());
-        Path uploadPath = Paths.get(FULL_PATH);
+        Path uploadPath = Paths.get(IMG_DATA_PATH);
         Path filePath = uploadPath.resolve(fileName);
         InputStream inputStream = file.getInputStream();
         Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
@@ -36,7 +36,7 @@ public class StorageService {
     public void deleteFile (String fileName) throws IOException {
         fileName = fileName.replaceAll("/img/data/", "");
         logger.info(fileName);
-        Path uploadPath = Paths.get(FULL_PATH);
+        Path uploadPath = Paths.get(IMG_DATA_PATH);
         Path filePath = uploadPath.resolve(fileName);
         Files.delete(filePath);
     }
