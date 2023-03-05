6e97a10deba92b16a9e6a822693057c23e67397d
Sebastian Alvarez <ca.sebastianlv@gmail.com>
2023-03-05 19:56:45 -0300

    Datatable: set weight

--- src/main/resources/static/js/datatable.js
+++ src/main/resources/static/js/datatable.js
@@ -2,6 +2,9 @@ $(document).ready(() => {
     $("#productos").DataTable(
         
         {   
+            "columnDefs": [
+                { "width": "15%", "targets": 1 }
+              ],
             "responsive": true,
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
